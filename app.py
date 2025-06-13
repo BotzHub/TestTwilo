c2bda6134a6554ef21d32c7f037e992e5fcddddc
Botz Hub <botzhub01@gmail.com>
2025-06-13 09:32:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 @app.route("/sms-handler", methods=["POST"])
 def sms_receive():
     from_number = request.form.get("From")
-    body = request.form.get("Body")
-    print(f"📩 SMS from {from_number}: {body}")
+    message = request.form.get("Body")
+    print(f"SMS from {from_number}: {message}")
     return "OK", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
